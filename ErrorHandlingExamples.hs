4414a8a0d835eb8aa6624590bdc35dc3896c4e16
Daniel Díaz <diaz.carrete@gmail.com>
2011-10-28 00:00:29 +0200

    New error handling example.

--- ErrorHandlingExamples.hs
+++ ErrorHandlingExamples.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE DeriveDataTypeable #-} 
+
 module ErrorHandlingExamples (
-        errorHandlingExample1
+        errorHandlingExample1,
+        errorHandlingExample2
 ) where
 
 import Control.Monad
 import Control.Monad.Error
--- import Control.Monad.Error.Class
 import Control.Monad.Identity
+import qualified Control.Exception as CE
+import qualified Control.Exception.Control as CEC
+import Data.Typeable
 
 -- Run with: runIdentity $ runErrorT $ errorHandlingExample1 7 1      
 errorHandlingExample1:: (Fractional a) => a -> a -> ErrorT String Identity a
@@ -18,3 +23,20 @@ errorHandlingExample1 nume deno = do
                             else return $ nume/deno 
                 handler = (\_->return 77)
 
+
+data FooException = FooException
+     deriving (Show, Typeable)
+
+instance CE.Exception FooException
+
+-- Catching IO exceptions inside ErrorT.
+-- Naive attempt that DOESN'T WORK. 
+-- The FooException passes through.
+-- Run with: runErrorT errorHandlingExample2
+errorHandlingExample2:: ErrorT String IO Int
+errorHandlingExample2 = 
+     let handler = \_ -> throwError "oops..." 
+     in
+        do
+           catchError (liftIO $ CE.throw FooException) handler
+           return 5
