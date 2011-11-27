8ddc5dc442d756c3bab90283e0f4b610c7554e1e
Daniel Díaz <diaz.carrete@gmail.com>
2011-11-27 23:15:41 +0100

    New examples in comments.

--- th-main.hs
+++ th-main.hs
@@ -10,11 +10,17 @@ import System.Environment
 --      http://haskell.org/ghc/docs/7.2.2/html/users_guide/template-haskell.html
 --      http://www.haskell.org/haskellwiki/Template_Haskell
 --      http://www.haskell.org/haskellwiki/Uniplate
+--      http://www.hyperedsoftware.com/blog/entries/first-stab-th.html
 
 -- Things to try in ghci:
 --      runQ [d| foo x = x + 1 |]
 --      runQ [d| foo x = x + 1 |] >>= print
 --      $(stringE . show =<< reify ''Int)
+--      runQ [| foldl |]
+--      runQ [| \x -> x |]
+--      runQ [| 1 + $([| 2 |]) |]
+--      runQ [t| IO Int |]
+--      runQ [d| f a = 1 |]
 
 $(iHateDelete
     [d|
