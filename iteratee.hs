9f37da9b3963d99821dd7e176e3a6594e6c0b785
Daniel Díaz <diaz.carrete@gmail.com>
2012-01-24 08:59:38 +0100

    Added new iteratee example.

--- iteratee.hs
+++ iteratee.hs
@@ -4,9 +4,14 @@ import           System.Console.GetOpt
 import           System.IO
 import           System.Environment
 import           System.Exit
+import qualified System.IO as SIO
+
+import           Data.ByteString
+import           Data.Word
 
 import qualified Data.Iteratee as I
-
+import qualified Data.Iteratee.IO.Handle as IOH
+import           Data.Iteratee.Base.ReadableChunk
 import           Data.Iteratee ((><>),(<><))
 import           Data.Iteratee.Char
 import qualified Data.Iteratee.Char as IC
@@ -18,3 +23,12 @@ main =
             it = (I.joinI $ (I.take 15 ><> I.take 10) I.stream2list)
        rs <- enum it >>= I.run
        print rs
+       -- Enumerating a handle over a finished iteratee doesn't read
+       -- from the handle, and doesn't throw an exception
+       handle <- openFile "iterdata/source.txt" ReadMode
+       let  
+            finishediter = I.idone () (I.EOF Nothing::I.Stream [Word8])
+       i <- IOH.enumHandle 22 handle finishediter >>= I.run
+       str <- SIO.hGetLine handle
+       SIO.putStr str
+       SIO.hClose handle
