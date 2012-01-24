36233afb4541652c5a17869445e451eacbab9380
Daniel Díaz <diaz.carrete@gmail.com>
2012-01-24 23:52:49 +0100

    Added another iteratee example.

--- iteratee.hs
+++ iteratee.hs
@@ -10,6 +10,7 @@ import           Data.ByteString
 import           Data.Word
 
 import qualified Data.Iteratee as I
+import qualified Data.Iteratee.ListLike as IL
 import qualified Data.Iteratee.IO.Handle as IOH
 import           Data.Iteratee.Base.ReadableChunk
 import           Data.Iteratee ((><>),(<><))
@@ -23,6 +24,15 @@ main =
             it = (I.joinI $ (I.take 15 ><> I.take 10) I.stream2list)
        rs <- enum it >>= I.run
        print rs
+       --
+       handle <- openFile "iterdata/source.txt" ReadMode
+       let  
+            enum2 = IL.take 20
+            it2 = I.joinI $ enum2 (I.stream2list::I.Iteratee ByteString IO [Word8])
+       i <- IOH.enumHandle 22 handle it2 >>= I.run
+       str <- SIO.hGetLine handle
+       SIO.putStr $ str ++ "\n"
+       SIO.hClose handle
        -- Enumerating a handle over a finished iteratee doesn't read
        -- from the handle, and doesn't throw an exception
        handle <- openFile "iterdata/source.txt" ReadMode
@@ -30,5 +40,5 @@ main =
             finishediter = I.idone () (I.EOF Nothing::I.Stream [Word8])
        i <- IOH.enumHandle 22 handle finishediter >>= I.run
        str <- SIO.hGetLine handle
-       SIO.putStr str
+       SIO.putStr $ str ++ "\n"
        SIO.hClose handle
